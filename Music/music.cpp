6e61d2f9a7f5ae1d671ef3f72e64a36187660c07
Bpalkmim <bpalkmim@gmail.com>
2015-12-08 23:28:12 -0200

    Mais pra função do music.

--- Music/music.cpp
+++ Music/music.cpp
@@ -27,7 +27,7 @@ Input * makeInput(Input * previous, int interval)
 		case 1:
 		case -11:
 			if (previous->accidental == SHARP) {
-				currentNote = (previous->note + 1) % 7;
+				currentNote = (Note) ((previous->note + 1) % 7);
 				currentAccidental = NATURAL;
 			}
 			else if (previous->accidental == NATURAL) {
@@ -44,16 +44,76 @@ Input * makeInput(Input * previous, int interval)
 					currentAccidental = SHARP;
 				}
 			}
-			else { // previoues->accidental == FLAT
+			else { // previous->accidental == FLAT
 				currentNote = previous->note;
 				currentAccidental = FLAT;
 			}
 			break;
 		case 2:
 		case -10:
+			if (previous->accidental == SHARP) {
+				if (previous->note == D) {
+					currentNote = F;
+					currentAccidental = NATURAL;
+				}
+				else if (previous->note == A) {
+					currentNote = C;
+					currentAccidental = NATURAL;
+				}
+				else { // C#, F# ou G#
+					currentNote = (Note) ((previous->note + 1) % 7);
+					currentAccidental = SHARP;
+				}
+			}
+			else if (previous->accidental == NATURAL) {
+				currentNote = (Note) ((previous->note + 1) % 7);
+				if (previous-> note == E || previous->note == B) {
+					currentAccidental = SHARP;
+				}
+				else {
+					currentAccidental = previous->accidental;
+				}
+			}
+			else { // previous->accidental == FLAT
+				currentNote = (Note) ((previous->note + 1) % 7);
+				if (previous-> note == E || previous->note == B) {
+					currentAccidental = NATURAL;
+				}
+				else {
+					currentAccidental = previous->accidental;
+				}
+			}
 			break;
 		case 3:
 		case -9:
+			if (previous->accidental == SHARP) {
+				currentNote = (Note) ((previous->note + 2) % 7);
+				if (previous->note == D || previous->note == A) {
+					currentAccidental = SHARP;
+				}
+				else { // C#, F# ou G#
+					currentAccidental = NATURAL;
+				}
+			}
+			else if (previous->accidental == NATURAL) {
+				currentNote = (Note) ((previous->note + 2) % 7);
+				if (previous-> note == C || previous->note == F || previous->note == G) {
+					currentAccidental = FLAT;
+				}
+				else {
+					currentAccidental = previous->accidental;
+				}
+			}
+			else { // previous->accidental == FLAT
+				if (previous-> note == E || previous->note == B) {
+					currentNote = (Note) ((previous->note + 2) % 7);
+					currentAccidental = previous->accidental;
+				}
+				else {
+					currentNote = (Note) ((previous->note + 1) % 7);
+					currentAccidental = NATURAL;
+				}
+			}
 			break;
 		case 4:
 		case -8:
@@ -72,9 +132,69 @@ Input * makeInput(Input * previous, int interval)
 			break;
 		case 9:
 		case -3:
+			if (previous->accidental == SHARP) {
+				currentNote = (Note) ((previous->note + 2) % 7);
+				if (previous->note == D || previous->note == A) {
+					currentAccidental = SHARP;
+				}
+				else { // C#, F# ou G#
+					currentAccidental = NATURAL;
+				}
+			}
+			else if (previous->accidental == NATURAL) {
+				currentNote = (Note) ((previous->note - 2 + 7) % 7);
+				if (previous-> note == E || previous->note == A || previous->note == B) {
+					currentAccidental = SHARP;
+				}
+				else {
+					currentAccidental = previous->accidental;
+				}
+			}
+			else { // previous->accidental == FLAT
+				if (previous-> note == E || previous->note == B) {
+					currentNote = (Note) ((previous->note + 2) % 7);
+					currentAccidental = previous->accidental;
+				}
+				else {
+					currentNote = (Note) ((previous->note + 1) % 7);
+					currentAccidental = NATURAL;
+				}
+			}
 			break;
 		case 10:
 		case -2:
+			if (previous->accidental == SHARP) {
+				currentNote = (Note) ((previous->note - 1 + 7) % 7);
+				if (previous-> note == C || previous->note == F) {
+					currentAccidental = NATURAL;
+				}
+				else {
+					currentAccidental = previous->accidental;
+				}
+			}
+			else if (previous->accidental == NATURAL) {
+				currentNote = (Note) ((previous->note - 1 + 7) % 7);
+				if (previous-> note == C || previous->note == F) {
+					currentAccidental = FLAT;
+				}
+				else {
+					currentAccidental = previous->accidental;
+				}
+			}
+			else { // previous->accidental == FLAT
+				if (previous->note == D) {
+					currentNote = B;
+					currentAccidental = NATURAL;
+				}
+				else if (previous->note == G) {
+					currentNote = E;
+					currentAccidental = NATURAL;
+				}
+				else { // Eb, Ab ou Bb
+					currentNote = (Note) ((previous->note - 1 + 7) % 7);
+					currentAccidental = FLAT;
+				}
+			}
 			break;
 		case 11:
 		case -1:
@@ -97,7 +217,7 @@ Input * makeInput(Input * previous, int interval)
 				}
 			}
 			else { // previous->accidental == FLAT
-				currentNote = (previous->note - 1 + 7) % 7;
+				currentNote = (Note) ((previous->note - 1 + 7) % 7);
 				currentAccidental = FLAT;
 			}
 			break;
@@ -117,98 +237,98 @@ Matrix<Input> * oneDimToTwoDim(Matrix<Input> * entry)
 		result[i] = new Input[entry->getNumCol()];
 		switch(entry->getContent()[i]->interval) {
 			case 0:
-					result[i][0].interval = 0;
-					result[i][1].interval = 0;
+				result[i][0].interval = 0;
+				result[i][1].interval = 0;
 				break;
 			case 1:
-					result[i][0].interval = 1;
-					result[i][1].interval = 0;
+				result[i][0].interval = 1;
+				result[i][1].interval = 0;
 				break;
 			case 2:
-					result[i][0].interval = 0;
-					result[i][1].interval = 1;
+				result[i][0].interval = 0;
+				result[i][1].interval = 1;
 				break;
 			case 3:
-					result[i][0].interval = 1;
-					result[i][1].interval = 1;
+				result[i][0].interval = 1;
+				result[i][1].interval = 1;
 				break;
 			case 4:
-					result[i][0].interval = 2;
-					result[i][1].interval = 1;
+				result[i][0].interval = 2;
+				result[i][1].interval = 1;
 				break;
 			case 5:
-					result[i][0].interval = 1;
-					result[i][1].interval = 2;
+				result[i][0].interval = 1;
+				result[i][1].interval = 2;
 				break;
 			case 6:
-					// Tratando sempre como quarta aumentada
-					result[i][1].interval = 0;
-					result[i][1].interval = 3;
+				// Tratando sempre como quarta aumentada
+				result[i][1].interval = 0;
+				result[i][1].interval = 3;
 				break;
 			case 7:
-					result[i][0].interval = 1;
-					result[i][1].interval = 3;
+				result[i][0].interval = 1;
+				result[i][1].interval = 3;
 				break;
 			case 8:
-					result[i][0].interval = 2;
-					result[i][1].interval = 3;
+				result[i][0].interval = 2;
+				result[i][1].interval = 3;
 				break;
 			case 9:
-					result[i][0].interval = 1;
-					result[i][1].interval = 4;
+				result[i][0].interval = 1;
+				result[i][1].interval = 4;
 				break;
 			case 10:
-					result[i][0].interval = 2;
-					result[i][1].interval = 4;
+				result[i][0].interval = 2;
+				result[i][1].interval = 4;
 				break;
 			case 11:
-					result[i][0].interval = 3;
-					result[i][1].interval = 4;
+				result[i][0].interval = 3;
+				result[i][1].interval = 4;
 				break;
 			case -1:
-					result[i][0].interval = -1;
-					result[i][1].interval = 0;
+				result[i][0].interval = -1;
+				result[i][1].interval = 0;
 				break;
 			case -2:
-					result[i][0].interval = 0;
-					result[i][1].interval = -1;
+				result[i][0].interval = 0;
+				result[i][1].interval = -1;
 				break;
 			case -3:
-					result[i][0].interval = -1;
-					result[i][1].interval = -1;
+				result[i][0].interval = -1;
+				result[i][1].interval = -1;
 				break;
 			case -4:
-					result[i][0].interval = 0;
-					result[i][1].interval = -2;
+				result[i][0].interval = 0;
+				result[i][1].interval = -2;
 				break;
 			case -5:
-					result[i][0].interval = -1;
-					result[i][1].interval = -2;
+				result[i][0].interval = -1;
+				result[i][1].interval = -2;
 				break;
 			case -6:
-					// Tratando sempre como quarta aumentada
-					result[i][0].interval = 0;
-					result[i][1].interval = -3;
+				// Tratando sempre como quarta aumentada
+				result[i][0].interval = 0;
+				result[i][1].interval = -3;
 				break;
 			case -7:
-					result[i][0].interval = -1;
-					result[i][1].interval = -3;
+				result[i][0].interval = -1;
+				result[i][1].interval = -3;
 				break;
 			case -8:
-					result[i][0].interval = -2;
-					result[i][1].interval = -3;
+				result[i][0].interval = -2;
+				result[i][1].interval = -3;
 				break;
 			case -9:
-					result[i][0].interval = -1;
-					result[i][1].interval = -4;
+				result[i][0].interval = -1;
+				result[i][1].interval = -4;
 				break;
 			case -10:
-					result[i][0].interval = -2;
-					result[i][1].interval = -4;
+				result[i][0].interval = -2;
+				result[i][1].interval = -4;
 				break;
 			case -11:
-					result[i][0].interval = -3;
-					result[i][1].interval = -4;
+				result[i][0].interval = -3;
+				result[i][1].interval = -4;
 				break;
 		}
 	}
